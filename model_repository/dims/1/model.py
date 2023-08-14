e6a79b20ac35b6dd1d15a2d174de0734f0d1ca0b
pl9632008 <67965957+pl9632008@users.noreply.github.com>
2023-08-14 10:35:22 +0800

    Update model.py

--- model_repository/dims/1/model.py
+++ model_repository/dims/1/model.py
@@ -4,11 +4,8 @@ import json
 import triton_python_backend_utils as pb_utils
 
 class TritonPythonModel:
-
-
     def initialize(self, args):
-
-
+        
         # You must parse model_config. JSON string is not parsed here
         model_config = json.loads(args['model_config'])
 
@@ -20,28 +17,20 @@ class TritonPythonModel:
         self.output0_dtype = pb_utils.triton_string_to_numpy(
             output0_config['data_type'])
             
-     
-    
     def execute(self, requests):
-
-
         output0_dtype = self.output0_dtype
-        
         responses = []
-
         for request in requests:
 
             in_0 = pb_utils.get_input_tensor_by_name(request, "dims_input")
             
             dims = in_0.as_numpy()
             
-            
             out_tensor_0 = pb_utils.Tensor("dims_output",
                                            dims.astype(output0_dtype))
             
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[out_tensor_0] 
-                
                 )
             responses.append(inference_response)
         return responses
